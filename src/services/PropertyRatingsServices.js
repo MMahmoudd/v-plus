e59520443518dd8a6e0fd9b7f57d3ed73ef30388
Ahmed Elwerdany <infoalwerdany@gmail.com>
2021-12-02 22:15:08 +0200

    property ratings & types crud systems added

--- src/services/PropertyRatingsServices.js
+++ src/services/PropertyRatingsServices.js
@@ -11,4 +11,36 @@ export default {
             }
         })
     },
+    fetchOneItem (id) {
+        return Service.get(`${resource}/property_ratings/${id}`)
+        .then((response) => {
+          if (response.status === 200) {
+              return response.data
+          }
+      })
+    },
+    addOneItem (data) {
+        return Service.post(`${resource}/property_ratings`, data)
+        .then((response) => {
+            if (response.status === 200) {
+                return response.data
+            }
+        })
+    },
+    updateOneItem (id, data) {
+        return Service.put(`${resource}/property_ratings/${id}`, data)
+        .then((response) => {
+            if (response.status === 200) {
+                return response.data
+            }
+        })
+    },
+    deleteOneItem (id) {
+        return Service.delete(`${resource}/property_ratings/${id}`)
+      .then((response) => {
+        if (response.status === 200) {
+            return response.data
+        }
+    })
+    },
 }
--- src/services/PropertyTypesServices.js
+++ src/services/PropertyTypesServices.js
@@ -11,4 +11,36 @@ export default {
             }
         })
     },
+    fetchOneItem (id) {
+        return Service.get(`${resource}/property_types/${id}`)
+        .then((response) => {
+          if (response.status === 200) {
+              return response.data
+          }
+      })
+    },
+    addOneItem (data) {
+        return Service.post(`${resource}/property_types`, data)
+        .then((response) => {
+            if (response.status === 200) {
+                return response.data
+            }
+        })
+    },
+    updateOneItem (id, data) {
+        return Service.put(`${resource}/property_types/${id}`, data)
+        .then((response) => {
+            if (response.status === 200) {
+                return response.data
+            }
+        })
+    },
+    deleteOneItem (id) {
+        return Service.delete(`${resource}/property_types/${id}`)
+      .then((response) => {
+        if (response.status === 200) {
+            return response.data
+        }
+    })
+    },
 }
