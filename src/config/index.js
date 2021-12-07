5f50225a9022eae2f5b5b2b606c6ccb2cd18aabc
Elteyab Hassan <Mr.elteb@gmail.com>
2021-12-07 09:06:01 +0200

    back to online API root url

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://taqeeem.millennium.sa/api/v1'
-export const API_URL = 'http://localhost:8000/api/v1'
+export const API_URL = 'https://taqeeem.millennium.sa/api/v1'
+// export const API_URL = 'http://localhost:8000/api/v1'
